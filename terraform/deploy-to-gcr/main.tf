98d39719a457277e4c000c9f4e630129335785e2
Vitaliy Stefanishin <Vitaliy.Stefanishin@itechart-group.com>
2022-04-18 00:42:42 +0300

    Deploy of myDrive app to GCR+S3

--- terraform/deploy-to-gcr/main.tf
+++ terraform/deploy-to-gcr/main.tf
@@ -1,13 +1,54 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.16.0"
+      }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# AWS S3 Bucket Creation
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_files = ["mycreds-aws"]
+}
+
+resource "aws_s3_bucket" "mydrive-vitstef" {
+  bucket = "mydrive-vitstef"
+  force_destroy = "true"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "mydrive-vitstef" {
+  bucket = aws_s3_bucket.mydrive-vitstef.bucket
+
+  rule {
+    bucket_key_enabled = false
+    apply_server_side_encryption_by_default {
+	sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_account_public_access_block" "mydrive-vitstef" {
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Google Part
+
 provider "google" {
   credentials = file ("mycreds-gcp.json")
   project = "just-student-344815"
 }
 
-data "google_secret_manager_secret" "qa" {
-  secret_id = "mydrive-vars"
-}
-
-resource "google_cloud_run_service" "default" {
+// Cloud Run deploy
+resource "google_cloud_run_service" "mydrive" {
   name     = "mydrive-terraform"
   location = "us-central1"
   template {
@@ -16,23 +57,23 @@ resource "google_cloud_run_service" "default" {
         image = "us-central1-docker.pkg.dev/just-student-344815/github-docker-repo/mydrive-app:latest"
 	env {
          name = "MONGODB_URL"
-         value = "mongodb://vitstef:VitStef89@cluster0-shard-00-00.nv76x.mongodb.net:27017,cluster0-shard-00-01.nv76x.mongodb.net:27017,cluster0-shard-00-02.nv76x.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-easmlr-shard-0&authSource=admin&retryWrites=true&w=majority"
+         value = var.mongodb-url
          }
         env {
           name = "KEY"
-          value = "1234567890"
+          value = var.key
           }
         env {
           name = "REMOTE_URL"
-          value = "http://localhost:3000"
+          value = var.remote-url
           }
         env {
           name = "DB_TYPE"
-          value = "mongo"
+          value = "s3"
           }
         env {
           name = "PASSWORD"
-          value = "1234567890"
+          value = var.password
           }
         env {
           name = "DISABLE_STORAGE"
@@ -56,15 +97,27 @@ resource "google_cloud_run_service" "default" {
           }
         env {
           name = "PASSWORD_ACCESS"
-          value = "1234567890"
+          value = var.password-access
           }
         env {
           name = "PASSWORD_REFRESH"
-          value = "1234567890"
+          value = var.password-refresh
           }
         env {
           name = "PASSWORD_COOKIE"
-          value = "1234567890"
+          value = var.password-cookie
+          }
+        env {
+          name = "S3_BUCKET"
+          value = "mydrive-vitstef"
+          }
+        env {
+          name = "S3_ID"
+          value = var.s3-id
+          }
+        env {
+          name  = "S3_KEY"
+          value = var.s3-key
           }
 
       ports {
@@ -73,17 +126,19 @@ resource "google_cloud_run_service" "default" {
       resources {
             limits   = {
                 cpu    = "2000m"
-                memory = "1024Mi"
+                memory = "1Gi"
       		}
       	   }      
       }
  
-      
     }
         metadata {
     annotations = {
       "autoscaling.knative.dev/minScale" = "2"
       "autoscaling.knative.dev/maxScale" = "2"
+      "run.googleapis.com/client-name"   = "cloud-console"
+      "run.googleapis.com/vpc-access-connector" = "projects/just-student-344815/locations/us-central1/connectors/mydrive-app-to-mongo-vm"
+      "run.googleapis.com/vpc-access-egress"    = "private-ranges-only"
     }
   } 
   }
@@ -119,10 +174,142 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+  location    = google_cloud_run_service.mydrive.location
+  project     = google_cloud_run_service.mydrive.project
+  service     = google_cloud_run_service.mydrive.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+// Network endpoint group creation
+resource "google_compute_region_network_endpoint_group" "mydrive" {
+  name                  = "mydrive-terraform"
+  network_endpoint_type = "SERVERLESS"
+  region                = "us-central1"
+  cloud_run {
+    service = google_cloud_run_service.mydrive.name
+  }
+}
+
+// Load Balancer creation
+// SSL Certificate import
+resource "google_compute_ssl_certificate" "mydrive-lb" {
+  name_prefix = "letsencrypt-"
+  description = "vitstef-ga"
+  private_key = file("/etc/letsencrypt/live/vitstef.ga/privkey.pem")
+  certificate = file("/etc/letsencrypt/live/vitstef.ga/fullchain.pem")
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+// LB Backend
+resource "google_compute_backend_service" "mydrive-lb-backend" {
+  affinity_cookie_ttl_sec = "0"
+
+  backend {
+    balancing_mode               = "UTILIZATION"
+    capacity_scaler              = "0"
+    group                        = google_compute_region_network_endpoint_group.mydrive.id
+    max_connections              = "0"
+    max_connections_per_endpoint = "0"
+    max_connections_per_instance = "0"
+    max_rate                     = "0"
+    max_rate_per_endpoint        = "0"
+    max_rate_per_instance        = "0"
+    max_utilization              = "0"
+  }
+
+  connection_draining_timeout_sec = "0"
+  enable_cdn                      = "false"
+  load_balancing_scheme           = "EXTERNAL"
+
+  log_config {
+    enable      = "false"
+    sample_rate = "0"
+  }
+
+  name             = "mydrive-lb-backend"
+  port_name        = "http"
+  project          = "just-student-344815"
+  protocol         = "HTTPS"
+  session_affinity = "NONE"
+  timeout_sec      = "30"
+}
+
+resource "google_compute_url_map" "mydrive-lb" {
+  default_service = google_compute_backend_service.mydrive-lb-backend.id
+  name            = "mydrive-lb"
+  project         = "just-student-344815"
+}
+
+
+// LB http-proxy
+resource "google_compute_target_http_proxy" "mydrive-lb-target-proxy" {
+  name       = "mydrive-lb-target-proxy"
+  project    = "just-student-344815"
+  proxy_bind = "false"
+  url_map    = google_compute_url_map.mydrive-lb.id
+}
+
+// LB https-proxy
+resource "google_compute_target_https_proxy" "mydrive-lb-target-proxy-2" {
+  name             = "mydrive-lb-target-proxy-2"
+  project          = "just-student-344815"
+  proxy_bind       = "false"
+  quic_override    = "NONE"
+  ssl_certificates = [google_compute_ssl_certificate.mydrive-lb.id]
+  url_map          = google_compute_url_map.mydrive-lb.id
+}
+
+// LB Frontend
+resource "google_compute_global_forwarding_rule" "mydrive-lb-forwarding-rule" {
+  ip_protocol           = "TCP"
+  ip_version            = "IPV4"
+  load_balancing_scheme = "EXTERNAL"
+  name                  = "mydrive-lb-forwarding-rule"
+  port_range            = "80-80"
+  project               = "just-student-344815"
+  target                = google_compute_target_http_proxy.mydrive-lb-target-proxy.id
+}
+
+resource "google_compute_global_forwarding_rule" "mydrive-lb-forwarding-rule-2" {
+  ip_protocol           = "TCP"
+  ip_version            = "IPV4"
+  load_balancing_scheme = "EXTERNAL"
+  name                  = "mydrive-lb-forwarding-rule-2"
+  port_range            = "443-443"
+  project               = "just-student-344815"
+  target                = google_compute_target_https_proxy.mydrive-lb-target-proxy-2.id
+}
+
+// DNS
+resource "google_dns_managed_zone" "vitstef-ga" {
+  description   = "Managed by Terraform"
+  dns_name      = "vitstef.ga."
+  force_destroy = "true"
+  name          = "vitstef-ga"
+  project       = "just-student-344815"
+  visibility    = "public"
+}
+
+// DNS records
+resource "google_dns_record_set" "http-vitstef-ga-A" {
+  managed_zone = google_dns_managed_zone.vitstef-ga.name
+  name         = "http.${google_dns_managed_zone.vitstef-ga.dns_name}"
+  project      = "just-student-344815"
+  rrdatas      = [google_compute_global_forwarding_rule.mydrive-lb-forwarding-rule.ip_address]
+  ttl          = "300"
+  type         = "A"
+}
+
+resource "google_dns_record_set" "mydrive-vitstef-ga-A" {
+  managed_zone = google_dns_managed_zone.vitstef-ga.name
+  name         = "mydrive.${google_dns_managed_zone.vitstef-ga.dns_name}"
+  project      = "just-student-344815"
+  rrdatas      = [google_compute_global_forwarding_rule.mydrive-lb-forwarding-rule-2.ip_address]
+  ttl          = "300"
+  type         = "A"
 }
 
